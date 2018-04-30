0e9f27f02314b96430acae070802ed1efe971aeb
nzpznk <skynnznk12@163.com>
2018-04-30 23:11:44 +0800

    merge & fix conflict

--- src/read_data.py
+++ src/read_data.py
@@ -42,45 +42,38 @@ puncpat = re.compile(r'[ -.\\:]')
 
 def read_to_database(indexlist):
     for i in indexlist:
-        if os.path.exists(config.data_file_list[i]) == false:
+        if os.path.exists(__database_dir__ + str(i)) == False:
             res = config.data_format.copy()
             with open(config.data_file_list[i], 'r', encoding='utf-8') as fp:
                 print('---', config.data_file_list[i], '---')
                 for line in fp.readlines():
-                    dat = re.match(datapat, line).groupdict()
-                    if not dat:
-                        print(line, 'can not be parsed')
+                    m = re.match(datapat, line)
+                    if not m:
                         continue
+                    dat = m.groupdict()
                     if not dat['id'] in config.contract_list:
-                        print(dat['id'], 'is not in constract list')
+                        # print(dat['id'], 'is not in constract list')
                         continue
                     dat['datetime'] = datetime.datetime(*[int(x) for x in re.split(puncpat, dat['datetime'] + '000')])
                     res[dat['id']].append(dat)
             with open(__database_dir__ + str(i), 'wb') as dbf:
                 pickle.dump(res, dbf)
-                dbf.close()
 
 def read_data(indexlist):
     res = config.data_format.copy()
+    tmp = config.data_format.copy()
     for i in indexlist:
-        with open(config.data_file_list[i], 'r', encoding = 'utf-8') as fp:
-            print('---', config.data_file_list[i], '---')
-            for line in fp.readlines():
-                m = re.match(datapat, line)
-                if not m:
-                    continue
-                dat = m.groupdict()
-                if not dat['id'] in config.contract_list:
-                    # print(dat['id'], 'is not in constract list')
-                    continue
-                dat['datetime'] = datetime.datetime(*[int(x) for x in re.split(puncpat, dat['datetime']+'000')])
-                res[dat['id']].append(dat)
-        with open(__database_dir__ + str(i), 'wb') as dbf:
-            pickle.dump(res, dbf)
-            dbf.close()
+        with open(__database_dir__ + str(i), 'rb') as dbf:
+            tmp = pickle.load(dbf)
+        for key in res:
+            res[key].append(tmp[key])
+    return res
 
 if __name__ == '__main__':
     # for test
     #read_data(range(3, 5)) # read three data file in the range
-    print(read_data(range(3, 5)))
+    read_to_database(range(3))
+    res = read_data(range(3))
+    if not res:
+        print('none')
     #print(res['B2'][2]['datetime'] - res['B2'][1]['datetime'])
